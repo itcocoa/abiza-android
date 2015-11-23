936a3c6744eca8ffa27edcb6157f01868ee65106
itcocoa <3216730556@qq.com>
2015-11-23 16:38:53 +0800

    增加欢迎屏幕倒数线程

--- app/src/main/java/cn/abiza/abiza_android/MainActivity.java
+++ app/src/main/java/cn/abiza/abiza_android/MainActivity.java
@@ -1,19 +1,57 @@
 package cn.abiza.abiza_android;
 
+import android.os.Handler;
+import android.os.Message;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
+    Handler MainHandler;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
+        MainHandler = new Handler()
+        {
+
+            @Override
+            public void handleMessage( Message msg) {
+                if ((String)msg.obj == "welcome_end"){
+                    if (islogined()){
+                        setContentView(R.layout.activity_main);
+                    }else{
+                        setContentView(R.layout.login);
+                    }
+                }
+            }
+
+        };
+
         setContentView(R.layout.welcome);
 
-        setContentView(R.layout.activity_main);
+        CountThread count_thd = new CountThread();
+        count_thd.start();
 
-        setContentView(R.layout.login);
+    }
 
+    private boolean islogined(){
+        return !true;
+    }
+
+    private class CountThread extends Thread
+    {
+        public void run()
+        {
+            try {
+                sleep(3000);
+                Message toMain = MainHandler.obtainMessage();
+                toMain.obj = "welcome_end";
+                MainHandler.sendMessage(toMain);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
     }
 }
