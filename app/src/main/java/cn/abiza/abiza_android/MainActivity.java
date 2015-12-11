4761ff4410e99b0c0ae11da3d281c8064a75c17b
itcocoa <3216730556@qq.com>
2015-12-11 16:16:55 +0800

    登录功能完成，调试成功

--- app/src/main/java/cn/abiza/abiza_android/MainActivity.java
+++ app/src/main/java/cn/abiza/abiza_android/MainActivity.java
@@ -1,11 +1,10 @@
 package cn.abiza.abiza_android;
 
-import android.content.Context;
+import android.content.SharedPreferences;
 import android.os.Handler;
 import android.os.Message;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
@@ -16,6 +15,8 @@ import org.json.*;
 public class MainActivity extends AppCompatActivity
 {
     Handler MainHandler;
+
+    public static final String USER_PREFS = "USER_PREFS";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,6 +30,7 @@ public class MainActivity extends AppCompatActivity
 
 
                 if (msg.obj instanceof UIProccessor){
+                    // Tools.exec() 回调
                     ((UIProccessor) msg.obj).proc();
                 }else{
                     if ((String)msg.obj == "welcome_end"){
@@ -64,14 +66,15 @@ public class MainActivity extends AppCompatActivity
         user_data.put("username", editText.getText());
         user_data.put("password",MD5Util.string2MD5(editText2.getText().toString()));
 
-        tool.exec("User","login",user_data,this.MainHandler,new UIProccessor(){
+        tool.exec("User", "login", user_data, this.MainHandler, new UIProccessor() {
             @Override
-            public void onDataloaded(JSONObject data){
+            public void onDataloaded(JSONObject data) {
                 try {
-                    if (data.getString("type").equals("error")){
-                        textView6.setText("登录失败"+data.getString("type"));
-                    }else{
-                        textView6.setText("登录成功"+data.getString("type"));
+                    if (data.getString("type").equals("error")) {
+                        textView6.setText("登录失败" + data.getString("type"));
+                    } else {
+                        textView6.setText("登录成功" + data.getString("type"));
+                        setLogined(true);
                     }
                 } catch (JSONException e) {
                     e.printStackTrace();
@@ -88,7 +91,20 @@ public class MainActivity extends AppCompatActivity
     }
 
     private boolean islogined(){
-        return !true;
+        SharedPreferences user_prefs = getSharedPreferences(USER_PREFS, 0);
+        boolean logined = user_prefs.getBoolean("logined", false);
+        return logined;
+    }
+
+    private boolean setLogined( boolean flag){
+        SharedPreferences user_prefs = getSharedPreferences(USER_PREFS, 0);
+        SharedPreferences.Editor editor = user_prefs.edit();
+        editor.putBoolean("logined", flag);
+
+        // Commit the edits!
+        editor.commit();
+
+        return flag;
     }
 
     private class CountThread extends Thread
